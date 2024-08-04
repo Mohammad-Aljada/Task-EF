27a2e40f286a17f6acb91a7761fd4242ab403566
Mohammad-Aljada <mohammadaljad.1234@gmail.com>
2024-08-04 15:03:24 +0300

    Task-EF

---
+++ Context/ApplicationDatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_EF.Model;
+
+namespace Task_EF.Context
+{
+    internal class ApplicationDatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            optionsBuilder.UseSqlServer("Server = .; Database = Task_EF ; Trusted_Connection=True; TrustServerCertificate=True");
+        }
+        public DbSet<Employee>Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
